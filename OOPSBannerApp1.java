c01d505c3645a334ec8ce7f9867fd460b0faf0b4
janani-0303 <jt9271@srmist.edu.in>
2026-02-17 20:57:19 +0530

    Added UC2: OOPS Banner

---
+++ OOPSBannerApp1.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display Application
+ * Displays the word "OOPS" in banner format using * and spaces.
+ * @author Developer
+ * @version 2
+ */
+
+public class OOPSBannerApp1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("  ***      ***      *****     ***** ");
+        System.out.println(" *   *    *   *     *    *    *     ");
+        System.out.println("*     *  *     *    *    *    *     ");
+        System.out.println("*     *  *     *    *****     ***** ");
+        System.out.println("*     *  *     *    *             * ");
+        System.out.println(" *   *    *   *     *             * ");
+        System.out.println("  ***      ***      *         ***** ");
+
+    }
+}
+
